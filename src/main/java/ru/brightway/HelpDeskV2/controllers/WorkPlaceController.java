eea011266b0a91b47feaa02570cba4d55f403e4c
Sphinx85 <91307559+Sphinx85@users.noreply.github.com>
2022-03-13 20:02:56 +0300

    Версия 0.2.2
    Кнопка деталей заявки.

--- src/main/java/ru/brightway/HelpDeskV2/controllers/WorkPlaceController.java
+++ src/main/java/ru/brightway/HelpDeskV2/controllers/WorkPlaceController.java
@@ -4,20 +4,23 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.brightway.HelpDeskV2.Entites.Message;
 import ru.brightway.HelpDeskV2.Entites.User;
 import ru.brightway.HelpDeskV2.services.interfaces.UserService;
 
 import java.security.Principal;
 
 @Controller
+
 @Data
 @RequestMapping("/workplace")
 public class WorkPlaceController {
 
     @Autowired
     private UserService userService;
+
+    private static Integer idMessage;
 
 
 
@@ -26,5 +29,20 @@ public class WorkPlaceController {
         User user = userService.findByUsername(principal.getName());
         model.addAttribute(user);
         return "current";
+    }
+
+    @PostMapping("/details")
+    public void messageId(@RequestBody Integer id){
+        idMessage = id;
+
+    }
+
+    @GetMapping("/details/{id}")
+    public String messageDetails(Principal principal, Model model, @PathVariable Integer idMessage){
+        User user = userService.findByUsername(principal.getName());
+        Message message = user.getMessages().get(idMessage);
+        model.addAttribute(message);
+
+        return "details";
     }
 }

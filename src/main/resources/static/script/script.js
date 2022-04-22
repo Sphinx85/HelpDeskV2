cd64f4002ad827a96a2d391abbe74b9f52d55294
psyabrprog <psyabrprog@mail.ru>
2022-04-22 23:13:01 +0300

    11/04/2022 19:19 button details

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -2,19 +2,47 @@ let toggleA = document.querySelector(".lex_card_button");
 console.log(toggleA);
 let detailCard = document.querySelector(".lex_card_text");
 console.log(detailCard);
+// let wrapperBlockCard = document.querySelector("left_block_down");
+// console.log(wrapperBlockCard);
 let a = 0;
 
 function openCloseDetails() {
   if (a == 0) {
-    console.log("a=1");
     detailCard.style.display = "block";
+    console.log(a);
     return (a = 1);
   } else {
     detailCard.style.display = "none";
-    console.log("a=0");
+    console.log(a);
     return (a = 0);
   }
-  // console.log(a);
 }
 
+// document.addEventListener("click", () => {
+//   var target = event.target;
+
+//   // if (target.tagName != "BUTTON")
+//   return console.log(toggleA);
+
+//   // target.style.background = "black";
+// });
+
 toggleA.addEventListener("click", openCloseDetails);
+
+// function openCloseDetails({ target }) {
+//   if (target.classList.contains("lex_card_button")) {
+//     let lex_card_text = target
+//       .closest(".lex_card")
+//       .querySelector(".lex_card_text");
+//     lex_card_text.hidden = !lex_card_text.hidden;
+
+//     // let lex_card_text = target
+//     //   .closest(".lex_card")
+//     //   .querySelector(".lex_card_text");
+//     // lex_card_text.style.display = "block";
+//   }
+// }
+
+// document
+//   .querySelector(".left_block_down")
+//   .addEventListener("click", openCloseDetails);
